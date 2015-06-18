0a83d2e73fb6cac6bc29329f31d48a8d6832c2a6
Jacqui <jacqui@mudbugmedia.com>
2015-06-18 17:42:46 -0500

    Add finished on to books --22:42

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618223310) do
+ActiveRecord::Schema.define(version: 20150618224204) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150618223310) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "rating"
+    t.date     "finished_on"
   end
 
 end
